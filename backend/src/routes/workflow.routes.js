06d8046865ea7817e9838f13ece7fe2368aee992
123sohails <sshaikh46597@gmail.com>
2026-08-08 00:13:42 +0530

    Fix workflow controller naming

--- backend/src/routes/workflow.routes.js
+++ backend/src/routes/workflow.routes.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const workflowcontroller = require("../controllers/workflow.controller");
-router.post('/create', workflowcontroller.createWorkflow);
-router.get("/", workflowcontroller.getWorkflows);
+
+const workflowController = require("../controllers/workflow.controller");
+
+router.post("/create", workflowController.createWorkflow);
+router.get("/", workflowController.getWorkflows);
 router.get("/:id", workflowController.getWorkflow);
 router.put("/:id", workflowController.updateWorkflow);
 router.delete("/:id", workflowController.deleteWorkflow);
+
 module.exports = router;
